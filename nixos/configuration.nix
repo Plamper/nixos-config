213a68bbec4b3c83c902b205283e9c9d894b001a
Felix Plamper <felix.plamper@stud.uni-hannover.de>
2023-07-28 21:17:44 +0200

    removed Pipewire because it's no longer supported

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -69,23 +69,7 @@
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
   
-  # Pipewire Configuration
-  security.rtkit.enable = true;
-    hardware.pulseaudio.enable = false;
-    services.pipewire = {
-      enable = true;
-      alsa.enable = true;
-      alsa.support32Bit = true;
-      pulse.enable = true;
-      jack.enable = true;
-      config.pipewire = {
-        "context.properties" = {
-          "default.clock.rate" = 48000;
-          "default.clock.allowed-rates" = [44100 48000 88200 96000 192000 768000 ];
-        
-        };
-      };
-    };
+  
   time.timeZone = "Europe/Berlin";
   
   environment.systemPackages = with pkgs; [
