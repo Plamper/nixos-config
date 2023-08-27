a34aae2dad9803fab2e7a6a03e74224266aea5d1
Felix Plamper <felix@plamper.com>
2023-08-27 15:16:34 +0200

    Added a fix for nautilus not being able to find Gstreamer Plugins

--- nixos/common/global/gnome.nix
+++ nixos/common/global/gnome.nix
@@ -1,4 +1,4 @@
-{ pkgs, ...}:
+{ pkgs, lib, ...}:
 {
    # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
@@ -9,4 +9,13 @@
     gnome.gnome-music
     gnome-tour
   ]);
+
+  # Nautilus Gstreamer is broken
+  # https://github.com/NixOS/nixpkgs/issues/195936#issuecomment-1278954466
+  environment.sessionVariables.GST_PLUGIN_SYSTEM_PATH_1_0 = lib.makeSearchPathOutput "lib" "lib/gstreamer-1.0" [
+    pkgs.gst_all_1.gst-plugins-good
+    pkgs.gst_all_1.gst-plugins-bad
+    pkgs.gst_all_1.gst-plugins-ugly
+    pkgs.gst_all_1.gst-plugins-libav
+  ];
 }
