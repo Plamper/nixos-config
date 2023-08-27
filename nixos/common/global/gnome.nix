85cf65a11e36a0c067e83f9f59e687111e72c38b
Felix Plamper <felix@plamper.com>
2023-08-27 15:21:22 +0200

    One Plugin name was wrong

--- nixos/common/global/gnome.nix
+++ nixos/common/global/gnome.nix
@@ -16,6 +16,6 @@
     pkgs.gst_all_1.gst-plugins-good
     pkgs.gst_all_1.gst-plugins-bad
     pkgs.gst_all_1.gst-plugins-ugly
-    pkgs.gst_all_1.gst-plugins-libav
+    pkgs.gst_all_1.gst-libav
   ];
 }
