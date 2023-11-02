46460cc0c5a2a0366f08e06262bb2b849062d491
Felix Plamper <felix@plamper.com>
2023-11-02 17:50:06 +0100

    Need to add electron 24.8.6 to allowed insecure Packages

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -45,6 +45,12 @@
       allowUnfree = true;
       # Workaround for https://github.com/nix-community/home-manager/issues/2942
       allowUnfreePredicate = (_: true);
+
+      # Fix for Heroic
+      permittedInsecurePackages = [
+        "electron-24.8.6"
+      ];
+
     };
   };
 
