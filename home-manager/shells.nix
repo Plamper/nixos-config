9b5066a57f4d7665f5422255f7ead98a3a6f1752
Felix Plamper <felix@plamper.com>
2023-11-27 22:57:40 +0100

    Changed wezterm Theme

--- home-manager/shells.nix
+++ home-manager/shells.nix
@@ -1,68 +1,74 @@
-{ pkgs, ...} : 
+{ pkgs, config, ... }:
 {
-  programs.bash.enable = true;
+  config = {
+    programs.bash.enable = true;
 
-  # programs.zsh = {
-  #   enable = true;
-  #   enableAutosuggestions = true;
-  #   enableCompletion = true;
-  #   # doesn't seem to work
-  #   enableVteIntegration = true;
-  #   autocd = true;
-  #   shellAliases = {
-  #     ll = "ls -l";
-  #     update-system = "cd /home/felix/Nix-Configuration && sudo nixos-rebuild switch --flake .#pc";
-  #     update-home = "cd /home/felix/Nix-Configuration && home-manager switch --flake .#felix@pc";
-  #     update-flake = "cd /home/felix/Nix-Configuration && nix flake update";
-  #   };
-  #   history = {
-  #     size = 10000;
-  #     path = "${config.xdg.dataHome}/zsh/history";
-  #   };
-  #   initExtra = ''bindkey "''${key[Up]}" up-line-or-search'';
-  # };
-  programs.fish = {
-    enable = true;
-    interactiveShellInit = ''
-      set fish_greeting # Disable greeting
-    '';
-    plugins = [
-      { name = "sponge"; src = pkgs.fishPlugins.sponge.src; }
-      { name = "z"; src = pkgs.fishPlugins.z.src; }
-    ];
-    shellAbbrs = {
-      update-system = "sudo nixos-rebuild switch --flake /home/felix/Nix-Configuration#(echo $hostname)";
-      update-flake = "nix flake update /home/felix/Nix-Configuration";
+    # programs.zsh = {
+    #   enable = true;
+    #   enableAutosuggestions = true;
+    #   enableCompletion = true;
+    #   # doesn't seem to work
+    #   enableVteIntegration = true;
+    #   autocd = true;
+    #   shellAliases = {
+    #     ll = "ls -l";
+    #     update-system = "cd /home/felix/Nix-Configuration && sudo nixos-rebuild switch --flake .#pc";
+    #     update-home = "cd /home/felix/Nix-Configuration && home-manager switch --flake .#felix@pc";
+    #     update-flake = "cd /home/felix/Nix-Configuration && nix flake update";
+    #   };
+    #   history = {
+    #     size = 10000;
+    #     path = "${config.xdg.dataHome}/zsh/history";
+    #   };
+    #   initExtra = ''bindkey "''${key[Up]}" up-line-or-search'';
+    # };
+    programs.fish = {
+      enable = true;
+      interactiveShellInit = ''
+        set fish_greeting # Disable greeting
+      '';
+      plugins = [
+        { name = "sponge"; src = pkgs.fishPlugins.sponge.src; }
+        { name = "z"; src = pkgs.fishPlugins.z.src; }
+      ];
+      shellAbbrs = {
+        update-system = "sudo nixos-rebuild switch --flake /home/felix/Nix-Configuration#(echo $hostname)";
+        update-flake = "nix flake update /home/felix/Nix-Configuration";
+      };
     };
-  };
 
-  programs.starship = {
-    enable = true;
-    # Configuration written to ~/.config/starship.toml
-    settings = {
-      add_newline = false;
+    programs.starship = {
+      enable = true;
+      # Configuration written to ~/.config/starship.toml
+      settings = {
+        add_newline = false;
 
-      character = {
-        success_symbol = "[➜](bold green)";
-        error_symbol = "[➜](bold red)";
-      };
-      c.symbol = " ";
-      git_branch.symbol = " ";
-      package.symbol = "󰏗 ";
-      hostname.ssh_symbol = " ";
-      rust.symbol = " ";
-      directory = {
-        truncation_length = 3;
-        truncation_symbol = "…/";
-        substitutions = {
-          "Documents" = "󰈙 ";
-          "Downloads" = " ";
-          "Music" = " ";
-          "Pictures" = " ";
-          "Nix-Configuration" = " ";
+        character = {
+          success_symbol = "[➜](bold green)";
+          error_symbol = "[➜](bold red)";
         };
+        c.symbol = " ";
+        git_branch.symbol = " ";
+        package.symbol = "󰏗 ";
+        hostname.ssh_symbol = " ";
+        rust.symbol = " ";
+        directory = {
+          truncation_length = 3;
+          truncation_symbol = "…/";
+          substitutions = {
+            "Documents" = "󰈙 ";
+            "Downloads" = " ";
+            "Music" = " ";
+            "Pictures" = " ";
+            "Nix-Configuration" = " ";
+          };
+        };
+        # package.disabled = true;
       };
-      # package.disabled = true;
+    };
+    home.packages = with pkgs;[ exa ];
+    home.shellAliases = {
+      ls = "exa --icons -F -H --group-directories-first --git -1";
     };
   };
 
--- home-manager/wezterm.nix
+++ home-manager/wezterm.nix
@@ -1,80 +1,114 @@
-{ pkgs, ...}:
+{ pkgs, ... }:
 {
   config = {
     programs.wezterm = {
       enable = true;
       extraConfig = ''
--- Pull in the wezterm API
-local wezterm = require 'wezterm'
+        -- Pull in the wezterm API
+        local wezterm = require 'wezterm'
 
--- This table will hold the configuration.
-local config = {}
+        -- This table will hold the configuration.
+        local config = {}
 
--- In newer versions of wezterm, use the config_builder which will
--- help provide clearer error messages
-if wezterm.config_builder then
-  config = wezterm.config_builder()
-end
+        -- In newer versions of wezterm, use the config_builder which will
+        -- help provide clearer error messages
+        if wezterm.config_builder then
+          config = wezterm.config_builder()
+        end
 
--- This is where you actually apply your config choices
+        -- This is where you actually apply your config choices
 
--- For example, changing the color scheme:
-config.color_scheme = 'GJM (terminal.sexy)'
-config.enable_wayland = false
-config.window_frame = {
-  -- The font used in the tab bar.
-  -- Roboto Bold is the default; this font is bundled
-  -- with wezterm.
-  -- Whatever font is selected here, it will have the
-  -- main font setting appended to it to pick up any
-  -- fallback fonts you may have used there.
-  font = wezterm.font { family = 'Cantarell', weight = 'Regular' },
+        -- For example, changing the color scheme:
+        config.color_scheme = 'adwaita-dark'
+        config.enable_wayland = false
+        config.window_frame = {
+          -- The font used in the tab bar.
+          -- Roboto Bold is the default; this font is bundled
+          -- with wezterm.
+          -- Whatever font is selected here, it will have the
+          -- main font setting appended to it to pick up any
+          -- fallback fonts you may have used there.
+          font = wezterm.font { family = 'Cantarell', weight = 'Regular' },
 
-  -- The size of the font in the tab bar.
-  -- Default to 10.0 on Windows but 12.0 on other systems
-  font_size = 12.0,
+          -- The size of the font in the tab bar.
+          -- Default to 10.0 on Windows but 12.0 on other systems
+          font_size = 12.0,
 
-  -- The overall background color of the tab bar when
-  -- the window is focused
-  active_titlebar_bg = '#333333',
+          -- The overall background color of the tab bar when
+          -- the window is focused
+          active_titlebar_bg = '#333333',
 
-  -- The overall background color of the tab bar when
-  -- the window is not focused
-  inactive_titlebar_bg = '#333333',
-}
+          -- The overall background color of the tab bar when
+          -- the window is not focused
+          inactive_titlebar_bg = '#333333',
+        }
 
-config.colors = {
-  tab_bar = {
-    -- The color of the inactive tab bar edge/divider
-    inactive_tab_edge = '#575757',
-  },
-}
-config.font = wezterm.font {
-  family = 'Fira Code',
-  weight = 'Medium',
-  harfbuzz_features = { 'cv02', 'ss01', 'ss03', 'ss05', 'ss07' },
-}
-config.font_size = 13.0;
+        config.colors = {
+          tab_bar = {
+            -- The color of the inactive tab bar edge/divider
+            inactive_tab_edge = '#575757',
+          },
+        }
+        config.font = wezterm.font {
+          family = 'Fira Code',
+          weight = 'Medium',
+          harfbuzz_features = { 'cv02', 'ss01', 'ss03', 'ss05', 'ss07' },
+        }
+        config.font_size = 13.0;
 
--- and finally, return the configuration to wezterm
-return config
+        -- and finally, return the configuration to wezterm
+        return config
       '';
+
+      colorSchemes = {
+        adwaita-dark = {
+          ansi = [
+            "#3d3846"
+            "#e01b24"
+            "#2ec27e"
+            "#ff7800"
+            "#1e78e4"
+            "#9841bb"
+            "#0ab9dc"
+            "#c0bfbc"
+          ];
+          brights = [
+            "#5e5c64"
+            "#f66151"
+            "#8ff0a4"
+            "#ffbe6f"
+            "#51a1ff"
+            "#c061cb"
+            "#4fd2fd"
+            "#f6f5f4"
+          ];
+          background = "#1E1E1E";
+          cursor_fg = "#1D1D1D";
+          cursor_border = "#BEAF8A";
+          cursor_bg = "#C0BFBC";
+          foreground = "#ffffff";
+          #selection_bg = "#444444";
+          #selection_fg = "#E9E9E9";
+        };
+      };
     };
-   
-    home.packages = let
-      xdgTermShim = pkgs.writeShellApplication {
-  name = "xdg-terminal-exec";
 
-  runtimeInputs = [ pkgs.wezterm ];
+    home.packages =
+      let
+        xdgTermShim = pkgs.writeShellApplication {
+          name = "xdg-terminal-exec";
 
-  text = ''
-    wezterm -e "$@"
-  '';
-}
-;
-    in [
-      xdgTermShim
-    ];
-    
+          runtimeInputs = [ pkgs.wezterm ];
+
+          text = ''
+            wezterm -e "$@"
+          '';
+        }
+        ;
+      in
+      [
+        xdgTermShim
+      ];
+
   };
 }
