666d87b99c9014a58d1578116ce855fd5615bf81
Felix Plamper <felix@plamper.com>
2024-04-29 16:28:00 +0200

    Fixed podman and distrobox

--- nixos/common/global/distrobox.nix
+++ nixos/common/global/distrobox.nix
@@ -1,9 +1,10 @@
-{ pkgs, config, ...}:
+{ pkgs, config, ... }:
 {
   config = {
     environment.systemPackages = with pkgs;[
       distrobox
     ];
     virtualisation.podman.enable = true;
+    virtualisation.containers.enable = true;
   };
 }
--- nixos/common/users/felix.nix
+++ nixos/common/users/felix.nix
@@ -1,6 +1,10 @@
 { pkgs, ...}:
 {
   # TODO: Configure your system-wide user settings (groups, etc), add more users as needed.
+  users.groups.felix = {
+    gid = 1000;
+  };
+  
   users.users = {
     # FIXME: Replace with your username
     felix = {
@@ -13,6 +17,7 @@
         # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
       ];
       # TODO: Be sure to add any other groups you need (such as networkmanager, audio, docker, etc)
+      group = "felix";
       extraGroups = [ "wheel" "networkmanager" ];
       description = "Felix Plamper";
       shell = pkgs.fish;
