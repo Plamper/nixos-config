75ac66212283ea8d82604b8b1a4fbe3b8703514f
Felix Plamper <felix.plamper@tuta.io>
2024-06-12 18:12:08 +0200

    Update Lanzaboote

--- nixos/common/global/kde.nix
+++ nixos/common/global/kde.nix
@@ -2,11 +2,11 @@
   # You may need to comment out "services.displayManager.gdm.enable = true;"
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
-  services.displayManager.sddm.wayland.enable = true;
+  # services.displayManager.sddm.wayland.enable = true;
 
   # needed because bugged currently
-  services.gnome.gnome-keyring.enable = true;
-  security.pam.services.sddm.enableGnomeKeyring = true;
+  # services.gnome.gnome-keyring.enable = true;
+  # security.pam.services.sddm.enableGnomeKeyring = true;
   networking.networkmanager.enable = true;
   services.power-profiles-daemon.enable = true;
 }
--- nixos/notebook/default.nix
+++ nixos/notebook/default.nix
@@ -26,9 +26,6 @@
     ../common/optional/virt-manager.nix
     ../common/optional/amd-graphics.nix
   ];
-  # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
 
   # networking.hostName = "nixos"; # Define your hostname.
   # Pick only one of the below networking options.
