a3ca621ebf0f94de25508556aada76b94568523f
Felix Plamper <felix@plamper.com>
2024-09-04 20:43:44 +0200

    Install VBox

--- nixos/common/optional/virt-manager.nix
+++ nixos/common/optional/virt-manager.nix
@@ -4,5 +4,9 @@
     virtualisation.libvirtd.enable = true;
     programs.virt-manager.enable = true;
     users.users."felix".extraGroups = [ "libvirtd" ];
+
+    virtualisation.virtualbox.host.enable = true;
+    users.extraGroups.vboxusers.members = [ "felix" ];
+    virtualisation.virtualbox.host.enableExtensionPack = true;
   };
 }
