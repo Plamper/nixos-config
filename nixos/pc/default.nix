875093f246292eb0a1a01f22ea8c35789c4dd7ed
Felix Plamper <felix@plamper.com>
2024-09-21 10:47:09 +0200

    Fix SMB mount

--- nixos/pc/default.nix
+++ nixos/pc/default.nix
@@ -43,14 +43,14 @@
 
 
   fileSystems."/home/felix/Music" = {
-    device = "//192.168.1.196/music";
+    device = "//192.168.1.196/Music";
     fsType = "cifs";
     options =
       let
         automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s,user,users";
 
       in
-      [ "${automount_opts},credentials=/etc/nixos/smb-secrets,uid=1000,gid=100" ];
+      [ "${automount_opts},credentials=/etc/nixos/smb-secrets,uid=1000,gid=1000" ];
     # or if you have specified `uid` and `gid` explicitly through NixOS configuration,
     # you can refer to them rather than hard-coding the values:
     # in ["${automount_opts},credentials=/etc/nixos/smb-secrets,${config.users.users.<username>.uid},gid=${config.users.groups.<group>.gid}"];
