34717e644290a26b013396bf11de752ba6c2e664
Felix Plamper <felix.plamper@tuta.io>
2025-03-06 21:47:41 +0100

    Fix nixd

--- home-manager/helix.nix
+++ home-manager/helix.nix
@@ -1,80 +1,89 @@
 { pkgs, ... }:
 {
-  config = {
-    programs.helix = {
-      enable = true;
-      defaultEditor = true;
-      settings = {
-        # theme = "adwaita-dark";
-        theme = "tokyonight_moon";
-        keys.normal = {
-          esc = [ "collapse_selection" "keep_primary_selection" ];
-        };
-        editor = {
-          cursor-shape = {
-            insert = "bar";
-            normal = "block";
-            select = "underline";
-          };
-          statusline = {
-            mode = {
-              normal = "NORMAL";
-              insert = "INSERT";
-              select = "SELECT";
-            };
-          };
-          end-of-line-diagnostics = "hint";
-          inline-diagnostics.cursor-line = "warning";
-        };
-      };
-      languages = {
-        language = [
-          {
-            name = "nix";
-            # language-servers = [ "nixd" "nil" ];
-            formatter = { command = "nixfmt"; };
-          }
-          {
-            name = "latex";
-            language-servers = [ "texlab" "harper-ls" ];
-          }
-          {
-            name = "markdown";
-            language-servers = [ "marksman" "harper-ls" ];
-            soft-wrap = { enable = true; };
-            formatter = {
-              command = "prettier";
-              args = [
-                "--parser"
-                "markdown"
-                "--prose-wrap"
-                "never"
-              ];
-            };
-          }
+  programs.helix = {
+    enable = true;
+    defaultEditor = true;
+    settings = {
+      # theme = "adwaita-dark";
+      theme = "tokyonight_moon";
+      keys.normal = {
+        esc = [
+          "collapse_selection"
+          "keep_primary_selection"
         ];
-        language-server = {
-          harper-ls = {
-            command = "harper-ls";
-            args = [ "--stdio" ];
+      };
+      editor = {
+        cursor-shape = {
+          insert = "bar";
+          normal = "block";
+          select = "underline";
+        };
+        statusline = {
+          mode = {
+            normal = "NORMAL";
+            insert = "INSERT";
+            select = "SELECT";
           };
         };
+        end-of-line-diagnostics = "hint";
+        inline-diagnostics.cursor-line = "warning";
       };
-      package = pkgs.unstable.helix;
     };
-    home.packages = with pkgs; [
-      wl-clipboard
-      wl-clipboard-x11
-      # LSP and tools that I want always available
-      marksman
-      texlab
-      unstable.harper
-      nil
-      nixd
-      nixfmt
-      nodePackages.prettier
-    ];
-
-
+    languages = {
+      language = [
+        {
+          name = "nix";
+          # language-servers = [ "nixd" "nil" ];
+          formatter = {
+            command = "nixfmt";
+          };
+        }
+        {
+          name = "latex";
+          language-servers = [
+            "texlab"
+            "harper-ls"
+          ];
+        }
+        {
+          name = "markdown";
+          language-servers = [
+            "marksman"
+            "harper-ls"
+          ];
+          soft-wrap = {
+            enable = true;
+          };
+          formatter = {
+            command = "prettier";
+            args = [
+              "--parser"
+              "markdown"
+              "--prose-wrap"
+              "never"
+            ];
+          };
+        }
+      ];
+      language-server = {
+        harper-ls = {
+          command = "harper-ls";
+          args = [ "--stdio" ];
+        };
+      };
+    };
+    package = pkgs.unstable.helix;
   };
+  home.packages = with pkgs; [
+    wl-clipboard
+    wl-clipboard-x11
+    # LSP and tools that I want always available
+    marksman
+    texlab
+    unstable.harper
+    nil
+    nixd
+    nixfmt-rfc-style
+    nodePackages.prettier
+  ];
 }
