2fc5657f04e4f406b7b4410d12bad08802ed3120
Felix Plamper <felix.plamper@tuta.io>
2025-03-06 21:48:28 +0100

    Fix Ibus

--- nixos/common/optional/ibus.nix
+++ nixos/common/optional/ibus.nix
@@ -1,12 +1,19 @@
-{ pkgs, lib, config, ... }:
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
 {
   config = {
     i18n.inputMethod = {
       enable = true;
       type = "ibus";
       ibus.engines = with pkgs.ibus-engines; [
+        pinyin
         libpinyin
       ];
     };
+    environment.variables.GTK_IM_MODULE = lib.mkForce "";
   };
 }
